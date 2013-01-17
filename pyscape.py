3dd93884648060b8b1ab084597867b191dae4024
Martin Doege <mdoege@compuserve.com>
2013-01-17 18:19:16 +0100

    open/save/start/stop buttons

--- pyscape.py
+++ pyscape.py
@@ -9,7 +9,8 @@ The sound files need to be mono, otherwise panning won't work!
 """
 
 from Tkinter import *
-import os, random, math
+from tkFileDialog import *
+import os, random, math, csv
 from random import random as r
 
 import openal
@@ -35,13 +36,69 @@ def update_title():
 w = Canvas(master, width=pix[0], height=pix[1], bg="white")
 w.pack()
 
+def save_file():
+	mypath = asksaveasfilename()
+	with open(mypath, 'wb') as csvfile:
+		wr = csv.writer(csvfile)
+		for p in par:
+			wr.writerow(p.getdata())
+			
+def load_file():
+	global par
+
+	for p in par:
+		n = p.n
+		p.active = False
+		p.play_or_stop()
+		w.delete("C%u" % n)
+		w.delete("T%u" % n)
+	par = []
+	mypath = askopenfilename()
+	try:
+		with open(mypath, 'rb') as csvfile:
+			wr = csv.reader(csvfile)
+			for row in wr:
+				act = (row[3] == 'True')
+				par.append(Source(int(row[0]), float(row[1]), float(row[2]), row[4], active = act))
+	except:
+		print "Cannot read file", mypath
+	stop_act()
+
+Button(master, text = "Save", command = save_file).pack(side = RIGHT)
+Button(master, text = "Load", command = load_file).pack(side = RIGHT)
+
+def start_act():
+	global stop_it
+	stop_it = False
+	but_on.config(state = DISABLED)
+	but_off.config(state = NORMAL)
+	for p in par:
+		p.play_or_stop()
+
+stop_it = False
+
+def stop_act():
+	global stop_it
+	stop_it = True
+	but_off.config(state = DISABLED)
+	but_on.config(state = NORMAL)
+	for p in par:
+		p.play_or_stop()
+
+but_on = Button(master, text = "Start", command = start_act, state = DISABLED)
+but_off = Button(master, text = "Stop", command = stop_act)
+
+but_on.pack(side = LEFT)
+but_off.pack(side = LEFT)
+
 par = []
 
 class Source():
-	def __init__(s, n, x, y, fn):
+	def __init__(s, n, x, y, fn, active = False):
 		cx, cy = x*pix[0], y*pix[1]
 		s.n = n
-		s.active = False
+		s.fn = fn
+		s.active = active
 		s.circ = w.create_oval(cx-cr, cy-cr, cx+cr, cy+cr, fill="white", tags="C%u" % n)
 		s.text = w.create_text(cx, cy, text = "%u" % n, tags="T%u" % n)
 		w.tag_bind("C%u" % n, "<Button-1>", s.clicked)
@@ -60,7 +117,10 @@ class Source():
 		w.tag_raise("T%u" % s.n)
 		s.active = not s.active
 		s.update_color()
-		if s.active:
+		s.play_or_stop()
+		
+	def play_or_stop(s):
+		if s.active and not stop_it:
 			s.source.play()
 		else:
 			s.source.stop()
@@ -90,6 +150,9 @@ class Source():
 		w.itemconfig("T%u" % s.n, fill=tc)
 		update_title()
 
+	def getdata(s):
+		return [s.n, 1.*s.x/pix[0], 1.*s.y/pix[1], s.active, s.fn]
+		
 for i in range(1, 8):
 	f = random.choice(fn)
 	par.append(Source(i, r()*.9, r()*.9, os.path.join(wpath, f)))
